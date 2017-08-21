8cbde141a515c925d5bafc0769ce77858567758a
zhangty12 <randomperm@outlook.com>
2017-08-21 15:29:16 +0800

    tune params of noise

--- init_noise.py
+++ init_noise.py
@@ -10,21 +10,21 @@ class init_noise(initiator):
         data = []
         ans = []
         for line in open('noise/noise.dat', 'r').readlines():
-            datum_str = line.split(' ')
+            datum_str = line.split()
             datum = [float(f) for f in datum_str]
-            data.append(datum[:4])
+            data.append(datum[:5])
             ans.append(datum[5])
 
         X = numpy.array(data)
         y = numpy.array(ans)
         X_normed = X / X.max(axis = 0)
 
-        saga_params = {'step_size': [0.005, 0.01, 0.05], \
-                        'temp': [3, 5, 7]}
-        svrg_params = {'step_size': [0.005, 0.01, 0.05], \
-                        'temp': [3, 5, 7]}
-        sgld_params = {'step_size': [0.00005, 0.0001, 0.0005]}
-        sald_params = {'step_size': [0.00005, 0.0001, 0.0005]}
+        saga_params = {'step_size': [0.005, 0.01, 0.5], \
+                        'temp': [5, 6, 7, 8, 9, 10, 15, 20]}
+        svrg_params = {'step_size': [0.005, 0.01, 0.5], \
+                        'temp': [5, 6, 7, 8, 9, 10, 15, 20]}
+        sgld_params = {'step_size': [0.0005, 0.001, 0.005]}
+        sald_params = {'step_size': [0.0005, 0.001, 0.005]}
 
-        return 'noise', X_normed, y, saga_params, svrg_params, \
+        return 'noise', 5, X_normed, y, saga_params, svrg_params, \
                         sgld_params, sald_params
